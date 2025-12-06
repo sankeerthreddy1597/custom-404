c9802866318b88465e730c82e8787a6116e6aefb
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2025-12-06 00:48:12 -0800

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",       
+  basePath: "/custom-404",
+  assetPrefix: "/custom-404",
   images: {
     unoptimized: true,     
   },
